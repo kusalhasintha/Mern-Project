e56bcfe0b6e7e04f35a9ad6fc63c01810373251a
Kusal K <k.kusal@directfn.com>
2026-01-19 10:44:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const loadTasks = async () => {
+    const res = await axios.get("http://localhost:5000/api/tasks");
+    setTasks(res.data);
+  };
+
+  const addTask = async () => {
+    if (!title) return;
+    await axios.post("http://localhost:5000/api/tasks", { title });
+    setTitle("");
+    loadTasks();
+  };
+
+  const deleteTask = async (id) => {
+    await axios.delete(`http://localhost:5000/api/tasks/${id}`);
+    loadTasks();
+  };
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      <h2>Mini MERN Task App</h2>
+
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Enter task"
+      />
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {tasks.map((t) => (
+          <li key={t._id}>
+            {t.title}
+            <button onClick={() => deleteTask(t._id)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
